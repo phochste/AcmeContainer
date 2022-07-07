faab705a6b528cbc25ac6e5228e214883da2e025
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-07-07 10:19:43 +0200

    Adding an example how to do an autocomplete selection

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,13 @@
 	import Login from './Login.svelte';
 	import Logout from './Logout.svelte';
 	import Container from './Container.svelte';
+	import ContainerSelect from './ContainerSelect.svelte';
+	import { DIR_TYPE, FILE_TYPE, type FileInfo } from './container';
 
 	export let name: string;
+
 	let profile: any;
+	let selectedResource : FileInfo;
 </script>
 
 <nav class="navbar navbar-default">
@@ -23,3 +27,11 @@
 <Login bind:profile={profile}/>
 
 <Container resource="https://patrickhochstenbach.net/"/>
+
+{#if selectedResource}
+ <h3>{selectedResource.url}</h3>
+{/if}
+<ContainerSelect 
+	resource="https://patrickhochstenbach.net/"
+	bind:selectedResource
+	/>
